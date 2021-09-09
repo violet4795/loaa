e6f49374e10bf40ee329fc4091ed65f4ee2edd6c
hyunsoo <violet4795@naver.com>
2021-09-09 17:42:43 +0900

    init

---
+++ src/components/main/main-header.jsx
@@ -0,0 +1,17 @@
+import { React, Component } from 'react'
+
+export default class MainHeader extends Component {
+  render() {
+    return (
+      <header className="App-header">
+        <div>
+          <span>휴식 게이지 관리</span>
+          <span>캐릭터 보기</span>
+          <span>추가예정</span>
+          <span></span>
+          <span></span>
+        </div>
+      </header>
+    )
+  }
+}
---
+++ src/layouts/default.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import CustomButton from './CustomButton'
+
+function WarningButton() {
+  // return React.createElement(CustomButton, {color: 'red'}, null);
+  return <CustomButton color="red" />
+}
